32d3d88dd6e52d4b8bfcba2e1c98308a7d61175b
Ota Tomomi <46tmm.bob@gmail.com>
2016-04-26 18:04:36 +0900

    4/26

--- src/jp/co/plusize/ota_tomomi/calculate_sales/readbranch.java
+++ src/jp/co/plusize/ota_tomomi/calculate_sales/readbranch.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FilenameFilter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.regex.Pattern;
 
@@ -11,7 +12,17 @@ import java.util.regex.Pattern;
 public class readbranch {
 	public static void main (String[] args){
 
+		//支店コード、商品コードをキーとして支店名・商品名を参照するマップリストの定義
 		HashMap<String,String> map = new HashMap<String,String>();
+
+		//支店コード、商品コードをキーとして売上金額を参照するマップリストの定義
+		HashMap<String,Long> revenuemap = new HashMap<String,Long>();
+
+
+		//ArrayList branchNumberList = new ArrayList();
+		//ArrayList commodityNumberList = new ArrayList();
+		ArrayList revenueList = new ArrayList();
+
 
 		//支店定義ファイルの読み込み
 		try{
@@ -26,17 +37,16 @@ public class readbranch {
 					String[] cols = sb.split(",");     //カンマによる文字列の分割
 
 					try{
-						//支店コードをキーとして支店名をマップリストに追加
-						map.put(cols[0],cols[1]);
-						System.out.println(map.get(cols[0]));
-
-
 						//支店定義ファイルのフォーマットが正しいかどうか判定（桁数、カンマの有無、半角数値）
 						if(cols[0].length() != 3 || cols.length > 2 ||  isNumber(cols[0])){
 							System.out.println("支店定義ファイルのフォーマットが不正です");
 							return;
 
 						}
+
+						//支店コードをキーとして支店名をマップリストに追加
+						map.put(cols[0],cols[1]);//支店名・商品名のマップ
+						revenuemap.put(cols[0],new Long(0));//売上金額のマップ
 
 
 					}
@@ -68,15 +78,17 @@ public class readbranch {
 				String[] cols = sc.split(",");     //カンマによる文字列の分割
 
 				    try{
-				    	//商品コードをキーとして商品名をマップリストに追加
-						map.put(cols[0],cols[1]);
-						System.out.println(map.get(cols[0]));
-
-						//商品定義ファイルのフォーマットが正しいかどうか判定（桁数、カンマの有無、半角英数値）
+				    	//商品定義ファイルのフォーマットが正しいかどうか判定（桁数、カンマの有無、半角英数値）
 						if(cols[0].length() != 8|| cols.length > 2 || isAlphabet(cols[0])){
 							System.out.println("商品定義ファイルのフォーマットが不正です");
 							return;
-							}
+						}
+
+				    	//商品コードをキーとして商品名をマップリストに追加
+						map.put(cols[0],cols[1]);//支店名・商品名のマップ
+						revenuemap.put(cols[0],new Long(0));//売上金額のマップ
+
+
 
 						}
 				    //商品名に改行が含まれたときの例外処理
@@ -96,28 +108,77 @@ public class readbranch {
 		}
 
 
-		//売上ファイルの検索(未完！！)
+		//売上ファイルの検索
 
 		File dir = new File(args[0]);
 		String[] files = dir.list(new MyFilter());
+
+
 		for(int i = 0; i < files.length; i++){
-			System.out.println(files[i]);
+			//   System.out.println(files[i]);
+
+			//売上ファイル読み込み
+			try{
+				File rcdfile = new File(args[0] + "\\" + files[i]);
+				FileReader rcdfr = new FileReader(rcdfile);
+				BufferedReader rcdbr = new BufferedReader(rcdfr);
+				String rcds;
+
+				//売上ファイルデータ集計
+				while((rcds = rcdbr.readLine()) != null){
+					revenueList.add(rcds);
+
+
+					}
+				for(int j = 0; j < revenueList.size(); j++){
+					System.out.println(revenueList.get(j));
+				}
+
+				rcdbr.close();
+			}
+			catch(IOException e){
+				System.out.println("予期せぬエラーが発生しました");
+
+			}
+			//売上集計
 		}
+
+
 }
 
 //半角数値以外のときtrue
 	public static boolean isNumber(String s) {
 	    	return Pattern.compile("\\D").matcher(s).find();
-}
+	}
 //半角英数値以外のときtrue
 	public static boolean isAlphabet(String s) {
 		return Pattern.compile("\\W").matcher(s).find();
 	}
-	//ファイルを検索するためのクラス(未完！！！)
-	class MyFilter implements FilenameFilter{
+//ファイルを検索するためのクラス(未完！！！)
+	public static class MyFilter implements FilenameFilter{
+		public boolean accept(File dir,String name){
+			int index = name.lastIndexOf(".");
+
+			//"."以下の文字列を取り出して小文字にする
+			String ext = name.substring(index + 1).toLowerCase();
+
+			//"."以前の文字列を取り出す
+			String fileName = name.substring(0,index);
 
 
-		}
+
+
+
+			//拡張子がrcdかつファイル名が8桁（連番の条件を入れたい）
+			if(ext.equals("rcd") == true && fileName.length() == 8){
+				return true;
+				}
+
+
+
+			return false;
+	}
+}
 }
 
 
@@ -126,7 +187,6 @@ public class readbranch {
 
 
 
-}
 
 
 
