b1cc0c277d9abfbedb4e9c436ad1ad3cb1d30a82
Ota Tomomi <46tmm.bob@gmail.com>
2016-05-24 14:10:10 +0900

    DB3

--- branch.sql
+++
@@ -1,6 +0,0 @@
-/*
--- Query: SHOW COLUMNS FROM bbs.branch
--- Date: 2016-05-24 13:34
-*/
-INSERT INTO `branch` (`Field`,`Type`,`Null`,`Key`,`Default`,`Extra`) VALUES ('id','int(11)','NO','PRI',NULL,'auto_increment');
-INSERT INTO `branch` (`Field`,`Type`,`Null`,`Key`,`Default`,`Extra`) VALUES ('name','varchar(45)','NO','',NULL,'');
--- position.sql
+++
@@ -1,6 +0,0 @@
-/*
--- Query: SHOW COLUMNS FROM bbs.position
--- Date: 2016-05-24 13:31
-*/
-INSERT INTO `position` (`Field`,`Type`,`Null`,`Key`,`Default`,`Extra`) VALUES ('id','int(11)','NO','PRI',NULL,'auto_increment');
-INSERT INTO `position` (`Field`,`Type`,`Null`,`Key`,`Default`,`Extra`) VALUES ('name','varchar(45)','NO','',NULL,'');
